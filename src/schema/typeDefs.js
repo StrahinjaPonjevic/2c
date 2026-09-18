f357256dfba3aafdad3bf339503ddc46e58416e0
Strahinja Ponjevic <strahinjaponjevic03@gmail.com>
2026-09-18 17:27:47 +0200

    feat: define base GraphQL schema

---
+++ src/schema/typeDefs.js
@@ -0,0 +1,15 @@
+export const typeDefs = `
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
+  type Query {
+    users: [User!]!
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!): User!
+  }
+`;
