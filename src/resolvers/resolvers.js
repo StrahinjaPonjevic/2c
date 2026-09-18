312b6e8fe4e1673a45e6f3b0dd7763b6924ca6f5
Strahinja Ponjevic <strahinjaponjevic03@gmail.com>
2026-09-18 18:02:31 +0200

    feat: setup Apollo server entry point

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -1,5 +1,5 @@
-import { randomUUID } from 'node:crypto'
-import { users } from '../data/users'
+import { randomUUID } from 'node:crypto';
+import { users } from '../data/users.js';
 
 export const resolvers = {
     Query: {
